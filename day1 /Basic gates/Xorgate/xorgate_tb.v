2bda9de9cb75b3ee88c44671e283c24901d03bba
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-14 09:51:56 +0530

    Create xorgate_tb.v

---
+++ day1 /Basic gates/Xorgate/xorgate_tb.v
@@ -0,0 +1,17 @@
+module xor_tb();
+reg a,b;
+wire out;
+xorgate uut(.a(a),.b(b),.out(out));
+initial begin 
+a=0;b=0;
+#10 a=0; b=1;
+#10 a=1; b=0;
+#10 a=1; b=1;
+#10$finish;
+end 
+initial begin 
+$dumpfile("xor_tb.vcd");
+$dumpvars();
+$monitor("%0t | A =%b | B=%b | OUT=%b",$time,a,b,out);
+end
+endmodule
