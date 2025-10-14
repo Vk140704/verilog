3097b19d4f37a5edf7a8c9924d0aef46ecc12eba
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-14 09:27:51 +0530

    Create Notgate_tb.v

---
+++ day1 /Basic gates/Notgate/Notgate_tb.v
@@ -0,0 +1,15 @@
+module not_tb();
+reg a;
+wire out;
+notgate uut(.a(a),.out(out));
+initial begin
+#10 a=0;
+#10 a=1;
+#10 $finish;
+end
+initial begin
+$dumpfile("not_tb.vcd");
+$dumpvars();
+$monitor("%0t | A=%b | out=%b ",$time,a,out);
+end
+endmodule
