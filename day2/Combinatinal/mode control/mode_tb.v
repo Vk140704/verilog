ac63c75cbc3fab745d392557afc50ed2de4bf513
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-14 19:24:30 +0530

    Create mode_tb.v

---
+++ day2/Combinatinal/mode control/mode_tb.v
@@ -0,0 +1,17 @@
+module mode_tb();                                                       
+reg a,b,control;                                                        
+wire out,cout;                                                          
+full_adder uut(a,b,control,out,cout);                                   
+initial begin                                                           
+#10 a=4'b0001; b=4'b1010; control=1;                                    
+#10 a=4'b0000; b=4'b1110; control=0;                                    
+#10 a=4'b1010; b=4'b0010; control=1;                                    
+#10 a=4'b1111; b=4'b0101; control=1;                                    
+#10 $finish;                                                            
+end                                                                     
+initial begin                                                           
+$dumpfile("mode_tb.vcd");                                               
+$dumpvars();                                                            
+$monitor("%0t | A=%b | B=%b | control=%b | OUT=%b | RIPPLE_OUT=%b ",$time,a,b,control,out,cout);                                                
+end                                                                     
+endmodule                                                               
