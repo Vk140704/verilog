dea84ff2f69d546b5fdebef1379e5987f7360dfe
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-14 11:10:29 +0530

    Create Bufifgate_tb.v

---
+++ day1 /Basic gates/Bufifgate/Bufifgate_tb.v
@@ -0,0 +1,17 @@
+module bufif_tb();
+reg a,control;
+wire y;
+bufifgate uut(.a(a),.control(control),.y(y));
+initial begin
+a=0; control=0;
+#10 a=0; control=1;
+#10 a=1; control=0;
+#10 a=1; control=1;
+#10 $finish;
+end
+initial begin
+$dumpfile("bufif_tb.vcd");
+$dumpvars();
+$monitor("%0t | A=%b | CONTROL=%b | Y=%b ",$time,a,control,y);
+end
+endmodule
