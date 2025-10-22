080bda8fe939e42054dd8107398196ecbbd896bb
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-22 10:18:20 +0530

    Add 4-to-1 multiplexer module in mux.v

---
+++ day4/mux4:1/mux.v
@@ -0,0 +1,12 @@
+module mux(input i0,i1,i2,i3,input[1:0]sel,output reg y);
+always @(*)begin
+ case(sel)
+  2'h0 : y = i0;
+  2'h1 : y = i1;
+  2'h2 : y = i2;
+  2'h3 : y = i3;
+  default: $display("invalid set input");
+ endcase
+ end
+endmodule
+
