629f49916a244b412c77b68b4c80a4e704a5ded4
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-10-22 15:23:52 +0530

    Add 2-to-1 demultiplexer module

---
+++ day4/Dmux1:2/dmux.v
@@ -0,0 +1,3 @@
+module dmux(input i,sel,output y0,y1);                                    
+assign {y0,y1}= sel?{1'b0,i}:{i,1'b0};                                    
+endmodule                            
