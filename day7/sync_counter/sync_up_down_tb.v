13e9bd96b04ad0717d51f61a2bd47aee02d069ad
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-12-04 12:07:13 +0530

    Update sync_up_down_tb.v

--- day7/sync_counter/sync_up_down_tb.v
+++ day7/sync_counter/sync_up_down_tb.v
@@ -8,9 +8,8 @@ module syncounter_tb();
   initial begin
     $dumpfile("syncounter_tb.vcd");
     $dumpvars();
-    $monitor(" Time=%0t | clk=%0b | rst=%0b | up=%0b | down=%0b | count=%0b",$time,clk,rst,up,down,count);
-    clk=0; rst=1;  down=0;
-    #4 up=1;
+    $monitor(" Time=%0t | clk=%b | rst=%b | up=%b | down=%b | count=%b",$time,clk,rst,up,down,count);
+    clk=0; rst=1;  down=0; up=1;
     #5 rst=0;
     #4 up=~up;
     #4 down=~down;
