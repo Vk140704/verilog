4de4e7781f8a97c54301ca81eb751895eebf8f16
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-12-08 00:02:59 +0530

    Create 1011_tb.v

---
+++ day8/Fsm /non_overlap/moore /1011_seq/1011_tb.v
@@ -0,0 +1,28 @@
+// Code your testbench here
+// or browse Examples
+module seq_1011_tb();
+  reg clk,rst,x;
+  wire z;
+  
+  seq_1011 dut(clk,rst,x,z);
+  always #5 clk = ~clk;
+  initial begin
+    $dumpfile("seq_1010_tb.vcd");
+    $dumpvars();
+    $monitor(" Time=%0t | clk=%b | rst=%b | x=%b | state=%h| z=%b",$time,clk,rst,x,dut.state,z);
+    
+    clk=0; rst=1; x=0;
+    #2rst=0;
+    #10 x=1;
+    #12 x=0;
+    #12 x=1;
+    #12 x=0;
+    #12 x=1;
+    #12 x=1;
+    #12 x=1;
+    #12 x=0;
+    #12 x=0;
+    #30$finish;
+    
+  end
+endmodule
