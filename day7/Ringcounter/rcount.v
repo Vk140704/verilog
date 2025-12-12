8fc97c87c3a382964eb3a9583886c0bd0e7379d4
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-12-12 11:41:01 +0530

    Create rcount.v

---
+++ day7/Ringcounter/rcount.v
@@ -0,0 +1,9 @@
+// Code your design here
+module ringcounter(input clk,rst, output reg [3:0]q);
+  always@(posedge clk or posedge rst)begin
+    if(rst)
+      q<=4'b1010;
+    else
+      q<={q[2:0],q[3]};
+  end
+endmodule 
